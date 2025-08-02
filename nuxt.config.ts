fc2ce4d758cb38036756ef164abe75143af98609
ZomJeedz <onenarok777@gmail.com>
2025-08-02 14:07:45 +0700

    chore: initialize project with Nuxt and Tailwind CSS
    - Added package.json with project metadata and dependencies for Nuxt, Tailwind CSS, and Vue.
    - Included favicon.ico for branding.
    - Created robots.txt to manage web crawler access.
    - Added tsconfig.json for TypeScript configuration in Nuxt.

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
